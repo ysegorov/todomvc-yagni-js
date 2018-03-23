ab1bcfba788358e4902f16f8c14619c330836635
Yuri Egorov <ysegorov@gmail.com>
2018-03-23 09:19:54 +0300

    +/- store rethinked using impure approach

--- src/js/store.js
+++ src/js/store.js
@@ -1,17 +1,32 @@
 
-import { always, callMethod2, filter, identity, ifElse, isNil, method, not, pick, pipe } from '@yagni-js/yagni';
+import { always, callMethod2, filter, identity, ifElse, isNil, method, mutate, not, pick, pipe } from '@yagni-js/yagni';
 
 import { debug } from './logger';
 import { storage } from './globals';
 
 
-const key = 'todos-yagni-js';
+const storageKey = 'todos-yagni-js';
+const todosKey = '_todos';
 
 const isCompleted = pick('completed');
+const length = pick('length');
 
+const allTodos = pick(todosKey);
+const activeTodos = pipe([
+  allTodos,
+  filter(not(isCompleted))
+]);
+const completedTodos = pipe([
+  allTodos,
+  filter(isCompleted)
+]);
 
-export const getAll = pipe([
-  always(key),
+const nextId = pipe([
+  allTodos,
+  length
+]);
+
+const loadFromStorage = pipe([
   method(storage, 'getItem'),
   ifElse(
     isNil,
@@ -20,19 +35,46 @@ export const getAll = pipe([
   )
 ]);
 
-export const getCompleted = pipe([
-  getAll,
-  filter(isCompleted)
-]);
-
-export const getActive = pipe([
-  getAll,
-  filter(not(isCompleted))
-]);
-
-export const save = callMethod2(
+const saveToStorage = callMethod2(
   always(storage),
   'setItem',
-  key,
+  storageKey,
   method(JSON, 'stringify')
 );
+
+function add(obj, todo) {
+  const todos = allTodos(obj);
+  const subj = mutate(obj, todosKey, todos.concat(todo));
+  return saveToStorage(allTodos(subj));
+}
+
+
+const storeProto = {
+  getAll: function getAll() {
+    //eslint-disable-next-line fp/no-this
+    return allTodos(this);
+  },
+  getActive: function getActive() {
+    //eslint-disable-next-line fp/no-this
+    return activeTodos(this);
+  },
+  getCompleted: function getCompleted() {
+    //eslint-disable-next-line fp/no-this
+    return completedTodos(this);
+  },
+  getNextId: function getNextId() {
+    //eslint-disable-next-line fp/no-this
+    return nextId(this);
+  },
+  addTodo: function addTodo(todo) {
+    //eslint-disable-next-line fp/no-this
+    return add(this, todo);
+  }
+};
+
+function createStore() {
+  const store = Object.create(storeProto);
+  return mutate(store, todosKey, loadFromStorage(storageKey));
+}
+
+export const store = createStore();
